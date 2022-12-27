504f125b0e866f2f822da6196fd391898bdc76c5
JayeshMalviya5 <115400817+JayeshMalviya5@users.noreply.github.com>
2022-12-27 12:49:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your code here
+let inputText = document.getElementById('evaluatedText');
+let count = document.getElementById('wordCount');
+let myarr = [];
+console.log(inputText,count);
+
+inputText.addEventListener('input', function countWord(){
+   let text = inputText.value.trim();
+    myarr = text.split(" ");
+    console.log(myarr.length);
+    count.innerText = myarr.length;
+    
+})
